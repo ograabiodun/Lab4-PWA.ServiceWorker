244a625d908bde5572de50a691a3f383f965635b
Mudi Ogra <ograabiodun@gmail.com>
2020-01-23 13:42:55 +0100

    service-worker image

--- app.js
+++ app.js
@@ -9,3 +9,33 @@ if  ('serviceWorker' in navigator) {
             console.log("Service Worker Failed to Register", err)
         }) 
 }
+
+//Function to perform HTTP request
+var get = function(url) {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    var result = xhr.responseText
+                    result = JSON.parse(result);
+                    resolve(result);
+                } else {
+                    reject(xhr);
+                }  
+            }
+        };
+        xhr.open("GET", url, true);
+        xhr.send();
+    });
+};
+
+
+get('https//api.nasa.gov/planetary/earth/imagery?api_key=fWfSMcDzyHfMuH3BW6jiIUBYaj3hKRyKBRTBqgEQ')
+    .then(function(response) {
+        console.log("Success", response);
+        document.getElementsByClassName('targetImage')[0].src = response.url;
+    })
+    .catch(function(err) {
+        console.log("Error", err);
+    })
