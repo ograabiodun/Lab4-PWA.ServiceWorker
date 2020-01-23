8c787697ebc4a107524f50e22cc6b6e15ac3a388
Mudi Ogra <ograabiodun@gmail.com>
2020-01-23 11:29:01 +0100

    service-worker change

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,22 @@
+var cacheName = 'v1';
+var cacheFiles = [
+    '../Lab4-PWA.ServiceWorker',
+    '../Lab4-PWA.ServiceWorker/index.html',
+    '../Lab4-PWA.ServiceWorker/app.js',
+    '../Lab4-PWA.ServiceWorker/service-worker.js'
+
+]
+
+
 self.addEventListener('install', function(e){
     console.log("[ServiceWorker] Installed")
+
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            console.log("[Service] Caching cacheFiles");
+            return cache.addAll(cacheFIles);
+        })
+    )
 })
 
 self.addEventListener('activate', function(e){
