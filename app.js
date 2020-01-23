a9df03a051c5c3644a3f7a400c83d9b5628c3ead
Mudi Ogra <ograabiodun@gmail.com>
2020-01-23 12:04:37 +0100

    service-worker changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if  ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('../Lab4-PWA.ServiceWorker/service-worker.js', {scope: '../Lab4-PWA.ServiceWorker/'})
         .then(function(registration) {
-            console.log("Service Worker Registered", registration);
+            console.log("Service Worker Registered");
         })
         .catch(function(err) {
             console.log("Service Worker Failed to Register", err)
