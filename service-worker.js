9a35abe6a9bfc6d13f09bcd8645191f16efd55a1
Mudi Ogra <ograabiodun@gmail.com>
2020-02-05 11:27:35 +0100

    new changes

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 var cacheName = 'v2';
 var cacheFiles = [
-    '/',
-    '/index.html',
-    '/app.js ',
-    '/service-worker.js',
-    '/manifest.json',
-    '/data/img/a-snake.jpg'
+    '../Lab4-PWA.ServiceWorker/',
+    '../Lab4-PWA.ServiceWorker/index.html',
+    '../Lab4-PWA.ServiceWorker/app.js ',
+    '../Lab4-PWA.ServiceWorker/service-worker.js',
+    '../Lab4-PWA.ServiceWorker/manifest.json',
+    '../Lab4-PWA.ServiceWorker/data/img/a-snake.jpg'
 ]
 
 
